15f3f3f1c4bcf5df4b92c8cea2964c1a96180ef7
Jayesh <jayeshdalal7@gmail.com>
2018-02-24 12:05:09 +0530

    sample of numpy modules

---
+++ numPyModule/BasicNumpy.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr = np.array([[1,2,3],[4,5,6]])
+print("Array  type " ,type(arr))
+print("No of dimesions" , arr.ndim)
+print("shape of array " , arr.shape)
+print("size of array" ,arr.size)
+print("type of the array" ,arr.dtype)
+
